0436317bcf73678b74986ccf7cac071a1661b822
YoheiHayamizu <yhayami1@binghamton.edu>
2023-01-20 16:10:34 -0500

    Update ros_noetic_installer.sh

--- ros_noetic_installer.sh
+++ ros_noetic_installer.sh
@@ -14,19 +14,22 @@ sudo apt install -y --no-install-recommends \
         git \
         kmod \
         libelf-dev \
-        cmake \
-        python3-rosdep \
-        python3-rosinstall \
-        python3-rosinstall-generator \
-        python3-wstool
+        cmake 
         
 # Installation
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt update
-sudo apt install ros-noetic-desktop-full
+sudo apt update -y
+sudo apt install ros-noetic-desktop-full -y
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+
+# Install some other dependencies
+sudo apt install -y \
+        python3-rosdep \
+        python3-rosinstall \
+        python3-rosinstall-generator \
+        python3-wstool
 
 # Initialize rosdep
 # Before you can use many ROS tools, you will need to initialize rosdep. 
