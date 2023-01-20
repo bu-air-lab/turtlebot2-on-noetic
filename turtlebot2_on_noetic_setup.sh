af829d125cb462325812e949fd48d18398f84ade
YoheiHayamizu <yhayami1@binghamton.edu>
2023-01-20 17:24:14 -0500

    Update turtlebot2_on_noetic_setup.sh

--- turtlebot2_on_noetic_setup.sh
+++ turtlebot2_on_noetic_setup.sh
@@ -13,15 +13,32 @@ sudo apt install -y \
         ros-noetic-base-local-planner \
         ros-noetic-move-base \
         ros-noetic-kobuki-ftdi \
-        ros-noetic-rgbd-launch
+        ros-noetic-rgbd-launch \
+        ros-melodic-openni2-launch \ 
+        ros-melodic-depthimage-to-laserscan
         
 sudo apt install -y \
         python3-vcstool \
         python3-pip \
         libusb-dev \
         libftdi-dev \
+        liborocos-kdl-dev \
         pyqt5-dev-tools
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws
 vcs import ./src < ~/turtlebot.rosinstall
+
+# Remove unnecessary and incompatible packages
+cd ./src
+mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers yujin_ocs/yocs_velocity_smoother ./
+rm -rf yujin_ocs
+mv linux_peripheral_interfaces/laptop_battery_monitor ./
+rm -rf linux_peripheral_interfaces
+# You need to MANUALLY, for now, apply the changes proposed in https://github.com/ros-drivers/linux_peripheral_interfaces/pull/18
+cd ../
+
+rosdep install --from-paths . --ignore-src -r -y
+
+# Build the packages
+catkin_make
