7fc266aff69844dc0669afd0f5f826dab85674fe
YoheiHayamizu <yhayami1@binghamton.edu>
2023-02-21 15:48:27 -0500

    Update turtlebot2_on_noetic_setup.sh

--- turtlebot2_on_noetic_setup.sh
+++ turtlebot2_on_noetic_setup.sh
@@ -13,7 +13,8 @@ sudo apt install -y \
         ros-noetic-base-local-planner \
         ros-noetic-move-base \
         ros-noetic-kobuki-ftdi \
-        ros-noetic-rgbd-launch
+        ros-noetic-rgbd-launch \ 
+        ros-noetic-libuvc-*
         
 sudo apt install -y \
         python3-vcstool \
@@ -33,18 +34,25 @@ vcs import ./src < ~/turtlebot2-on-noetic/turtlebot2.rosinstall
 cd ./src
 mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers yujin_ocs/yocs_velocity_smoother .
 rm -rf yujin_ocs
+
 mv linux_Peripheral_interfaces/laptop_battery_monitor .
 sed -i 's/env python/env python3/' ./laptop_battery_monitor/scripts/laptop_battery.py  # Change python interpreter from 2 to 3.
-# Set environment variable TURTLEBOT_BATTERY correctly.
-echo 'export TURTLEBOT_BATTERY=/sys/class/power_supply/BAT1' >> ~/.bashrc
+echo 'export TURTLEBOT_BATTERY=/sys/class/power_supply/BAT1' >> ~/.bashrc  # Set environment variable TURTLEBOT_BATTERY correctly.
 rm -rf linux_Peripheral_interfaces
+
 # You need to MANUALLY, for now, apply the changes proposed in https://github.com/ros-drivers/linux_peripheral_interfaces/pull/18
 rm -rf turtlebot_create_desktop/create_gazebo_plugins
 cd ../
 
+# Astro camera setup
+roscd astra_camera
+./scripts/create_udev_rules
+
+cd ~/catkin_ws
+
+rosdep install --from-paths . --ignore-src -r -y
 echo "The errors related to the python-orocos-kdl package can be ignored."
 echo "Refer to https://github.com/yujinrobot/kobuki/issues/427"
-rosdep install --from-paths . --ignore-src -r -y
 
 # Build the packages
 catkin_make
